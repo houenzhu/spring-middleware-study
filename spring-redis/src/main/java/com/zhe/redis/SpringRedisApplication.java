e3b83da6813104190b68bb1ade7d40acc46c3ccb
houenzhu <759959801@qq.com>
2025-10-28 16:06:00 +0800

    初始化redis项目

---
+++ spring-redis/src/main/java/com/zhe/redis/SpringRedisApplication.java
@@ -0,0 +1,11 @@
+package com.zhe.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRedisApplication.class, args);
+    }
+}
